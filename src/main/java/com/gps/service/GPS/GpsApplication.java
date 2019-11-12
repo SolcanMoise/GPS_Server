8b39b7e4db9920905ff40dd0c3ffeb93d772dda5
Solcan Moise <moise.solcan@paddypowerbetfair.com>
2019-11-12 21:14:05 +0200

    Save an entity into login table - just for demo purpose

--- src/main/java/com/gps/service/GPS/GpsApplication.java
+++ src/main/java/com/gps/service/GPS/GpsApplication.java
@@ -1,14 +1,28 @@
 package com.gps.service.GPS;
 
+import com.gps.service.GPS.models.Login;
+import com.gps.service.GPS.repository.LoginRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GpsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GpsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GpsApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner demo(LoginRepository repository) {
+        return (args) -> {
+            // save a few login info
+            Login login = new Login();
+            login.setUsername("username");
+            login.setPassword("password");
+            repository.save(login);
+        };
+    }
 
 }
